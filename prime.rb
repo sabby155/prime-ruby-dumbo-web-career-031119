46961cb80c74f7fbbf2cbda07fb4b3ad1bdf4de6
Sabrina Chowdhury <nebula155@gmail.com>
2019-02-04 18:21:15 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,5 @@
 # Add  code here!
 
-# def prime?(int)
-#   if int <= 1
-#     return false 
-#   elsif (2..(int - 1)).each do |n|
-#     return false if int % n == 0
-#   else 
-#   return true
-# end 
-  
   def prime?(int)
     if int <= 1
     return false
